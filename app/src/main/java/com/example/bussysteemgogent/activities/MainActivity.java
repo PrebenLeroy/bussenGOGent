c3a8c12cec4e543bf35e0ad27e4e067bf89f95d5
PrebenLeroy <preben.leroy@live.be>
2018-02-01 13:07:58 +0100

    add log out icon

--- app/src/main/java/com/example/bussysteemgogent/activities/MainActivity.java
+++ app/src/main/java/com/example/bussysteemgogent/activities/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -37,6 +39,13 @@ public class MainActivity extends AppCompatActivity {
                 viewDialog();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
     }
 
     private void viewDialog() {
